f473a28ad0977e02e69c8c8df39b84efe92965e4
Ashharja <rockashhar10@gmail.com>
2024-10-02 19:46:39 +0530

    Added Forgot and Reset Password Functionality

--- Backend/routes/authroute.js
+++ Backend/routes/authroute.js
@@ -5,9 +5,14 @@ import {
   signup,
   verifyEmail,
   forgotPassword,
+  resetPassword,
+  checkAuth,
 } from "../controllers/authcontroller.js";
+import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
+
+router.get("/check-auth", verifyToken, checkAuth);
 
 router.post("/signup", signup);
 
@@ -17,5 +22,7 @@ router.post("/logout", logout);
 
 router.post("/verify-email", verifyEmail);
 router.post("/forgot-password", forgotPassword);
+
+router.post("/reset-password/:token", resetPassword);
 
 export default router;
